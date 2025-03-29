9c567943816cc7f23b4c7eb9c301246bb029028d
giannischiout <giannis.chiout@gmail.com>
2025-03-29 13:04:52 +0200

    :sparkles: feat: (task): updated UI details

--- src/app/_sections/tasks/tasks-kanban-board/kanban-card-content.tsx
+++ src/app/_sections/tasks/tasks-kanban-board/kanban-card-content.tsx
@@ -1,7 +1,7 @@
 export function CardContent({ title, description }: { title: string; description: string }) {
   return (
     <div className="flex flex-col gap-0.5 px-0.5 py-2.5">
-      <p className="select-none tracking-wide text-[14px]">{title}</p>
+      <p className="select-none tracking-wide text-[14px] max-w-[22ch] truncate text-ellipsis ">{title}</p>
       <p className="line-clamp-2 select-none overflow-hidden  text-sm font-light text-gray-500">{description}</p>
     </div>
   )
